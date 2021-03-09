f82482a968317683f7966b528fe7365f36478fcd
Aldo Sotolongo (aldenso) <aldenso@gmail.com>
2021-03-08 22:35:29 -0500

    [devops] Adjusted groovy file

--- devops/dev/Jenkinsfile.groovy
+++ devops/dev/Jenkinsfile.groovy
@@ -1,26 +1,24 @@
-pipeline {
-    agent any
-    environment {
-        TF_VAR_prefix = "${params.PREFIX}"
-        TF_VAR_size = "${params.SIZE}"
-    }
-    stages {
+try {
+    node {
+        env.TF_VAR_prefix = "${params.PREFIX}"
+        env.TF_VAR_size = "${params.SIZE}"
+
         stage('Stage 1') {
-            steps {
-                withCredentials([string(credentialsId: 'AZTENANT-DEV', variable: 'AZTENANT'),
-                                string(credentialsId: 'AZSUBSCRIPTION-DEV', variable: 'AZSUBSCRIPTION'),
-                                string(credentialsId: 'AZCLIENT-DEV', variable: 'AZCLIENT'),
-                                file(credentialsId: 'AZCERTIFICATE-DEV', variable: 'AZCERTIFICATE')]) {
-                                environment {
-                                    ARM_CLIENT_ID = "${AZCLIENT}"
-                                    ARM_CLIENT_CERTIFICATE_PATH = "${AZCERTIFICATE}"
-                                    ARM_SUBSCRIPTION_ID = "${AZSUBSCRIPTION}"
-                                    ARM_TENANT_ID = "${AZTENANT}"
-                                }
-                    terraform init
-                    terraform plan
+            withCredentials([string(credentialsId: 'AZTENANT-DEV', variable: 'AZTENANT'),
+                            string(credentialsId: 'AZSUBSCRIPTION-DEV', variable: 'AZSUBSCRIPTION'),
+                            string(credentialsId: 'AZCLIENT-DEV', variable: 'AZCLIENT'),
+                            file(credentialsId: 'AZCERTIFICATE-DEV', variable: 'AZCERTIFICATE')]) {
+                env.ARM_CLIENT_ID = "${AZCLIENT}"
+                env.ARM_CLIENT_CERTIFICATE_PATH = "${AZCERTIFICATE}"
+                env.ARM_SUBSCRIPTION_ID = "${AZSUBSCRIPTION}"
+                env.ARM_TENANT_ID = "${AZTENANT}"
+                terraform init
+                terraform plan
                 }
-            }
         }
+    }
+} catch (Exception e) {
+    node {
+        throw e
     }
 }
